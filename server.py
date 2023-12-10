8871c2206fe0e49bcda80a624aa14b84f85be0aa
陈表祥 <3209823081@qq.com>
2023-12-10 11:37:11 +0800

    aa

--- server.py
+++ server.py
@@ -97,17 +97,13 @@ def handle_get(request_data,client_socket):
     # Splitting the path and handling query parameters
     path, _, query_string = full_path.partition('?')
     query_params = parse_query_string(query_string)
+   
 
     file_path = os.path.join(root_directory, path.lstrip('/'))
     print(file_path)
 
     # Check if the SUSTech-HTTP query parameter is present
     sustech_http_param = query_params.get('SUSTech-HTTP', None)
-
-    if sustech_http_param is None:
-        # If SUSTech-HTTP parameter is missing, return 400 Bad Request
-        response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
-        return response.encode('utf-8')
 
     if not os.path.exists(file_path):
         print('not exist')
@@ -120,17 +116,17 @@ def handle_get(request_data,client_socket):
         # If the requested path is a directory
         if sustech_http_param == '0':
             # SUSTech-HTTP=0: return an HTML page showing the file tree
-            directory_listing = generate_directory_listing(file_path)
+            directory_listing = generate_directory_listing(file_path,path)
             response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(directory_listing)}\r\n\r\n{directory_listing}'
-            print(response)
+            
         elif sustech_http_param == '1':
             # SUSTech-HTTP=1: return a list of files in the directory
             files_list = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
             print(files_list)
-            response = f'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {len(files_list)}\r\n\r\n{json.dumps(files_list)}'
-            print(response)
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length:{len(json.dumps(files_list))}\r\n\r\n{json.dumps(files_list)}'
+            
         else:
-            # Invalid value for SUSTech-HTTP parameter
+            # Invalid value for SUSTech-HTTP par ameter
             response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
     elif os.path.isfile(file_path):
         print('is file')
@@ -140,7 +136,9 @@ def handle_get(request_data,client_socket):
             content_type = get_content_type(file_path)
             response = f'HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(file_content)}\r\n\r\n'
             response = response.encode('utf-8') + file_content
+            return client_socket.send(response)
     else:
+        print("aaaaaa")
         # If the path does not exist, return 404 Not Found
         response = 'HTTP/1.1 404 Not Found\r\n\r\n'
 
@@ -162,18 +160,137 @@ def get_content_type(file_path):
     # Default to binary data if the file extension is not recognized
     return mime_types.get(file_extension, 'application/octet-stream')
 
+def generate_directory_listing(directory_path,path):
+    # Get the list of files and subdirectories in the directory
+    entries = os.listdir(directory_path)
+   
 
-def generate_directory_listing(directory_path):
-    # Generate an HTML page showing the file tree of the directory
-    directory_listing = f'<h1>Index of {directory_path}</h1>'
-    directory_listing += '<ul>'
-    for file in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, file)
-        if os.path.isdir(file_path):
-            file += '/'
-        directory_listing += f'<li><a href="{file}">{file}</a></li>'
-    directory_listing += '</ul>'
-    return directory_listing.encode('utf-8')
+    # Create an HTML page with links to files and subdirectories
+    html_content = '<html><body><ul>'
+    
+    # Add a link to the parent directory (if not the root directory)
+    if path != '/':
+        parent_link = f'<li><a href="../?SUSTech-HTTP=0">Parent Directory</a></li>'
+        html_content += parent_link
+        root_directory=f'<li><a href="../?SUSTech-HTTP=0">root Directory</a></li>'
+        html_content +=root_directory
+
+    for entry in entries:
+        entry_path = os.path.join(directory_path, entry)
+        if os.path.isdir(entry_path):
+            # For directories, add a trailing slash
+            entry_link = f'<li><a href="{entry}/?SUSTech-HTTP=0">{entry}/</a></li>'
+        else:
+            entry_link = f'<li><a href="{entry}">{entry}</a></li>'
+        html_content += entry_link
+
+    html_content += '</ul></body></html>'
+    return html_content
+
+def check_valid_post(path):
+    print(f'path is {path}')
+    splited_path = path.split('?')
+    result = False
+    if len(splited_path) != 2:
+        result = False
+        print('illeagl length')
+        return result
+    if splited_path[0] != "/delete" and splited_path[0] != "/upload":
+        print(f'not upload nor delete {splited_path[0]}')
+        result = False
+        return result
+    if "path=" not in splited_path[1]:
+        print(f'no path= {splited_path[1]}')
+        result = False
+        return result
+    result = True
+    return result
+
+def handle_upload(client_socket, headers, path, request_data):
+    # 构建用户目录的绝对路径
+    file_path = path.split('=')[1]
+    # print()
+    user_directory = os.path.join('data/', file_path)
+    print(user_directory)
+    if not os.path.exists(user_directory):
+        # 如果目标目录不存在，则返回404 Not Found
+        response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+        client_socket.send(response.encode('utf-8'))
+        print('not find filepath')
+        return
+    print(f'filepath is {path}')
+    # 从客户端接收文件内容
+    # print(request_data)
+
+    body_entity = request_data.split('\r\n\r\n')
+    # print(f'bodyentity is {body_entity}')
+    part1 = body_entity[1]
+    # print(f'part1 is {part1}')
+    name_line = part1.split('\r\n')[1]
+    print(f'nameline is {name_line}')
+    part2 = body_entity[2]
+    # print(f'part2 is {part2}')
+    content_line = part2.split('\r\n')[0]
+    print(f'contentline is {content_line}')
+    # 查找关键字 "filename="
+    filename_index = name_line.find('filename=')
+    filename = ''
+    if filename_index != -1:
+        # 从关键字 "filename=" 后面的位置开始查找值的起始位置
+        value_start = filename_index + len('filename="')
+
+        # 从起始位置开始查找值的结束位置
+        value_end = name_line.find('"', value_start)
+
+        # 提取值
+        filename = name_line[value_start:value_end]
+
+    else:
+        print("Filename not found.")
+
+    # # 将文件保存到用户目录
+    file_path = os.path.join(user_directory, filename)
+    with open(file_path, 'wb') as file:
+        file.write(content_line.encode('utf-8'))
+
+    response_headers = f'HTTP/1.1 200 OK\r\nSet-Cookie: session-id={000}; Path=/\r\n\r\n'
+    client_socket.send(response_headers.encode('utf-8'))
+    return
+
+def handle_delete(client_socket, headers, path, request_data):
+    return
+
+def handle_post(client_socket, headers, path, request_data):
+    try:
+        # 解析查询参数
+        splited_path = path.split('?')
+        if not check_valid_post(path):
+            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            print('bad 401')
+            client_socket.send(response.encode('utf-8'))
+            return
+        # print('aaa')
+        # print(path)
+        # print(path.split('?'))
+        query_params = path.split('?')[1]
+        post_type = path.split('?')[0]
+        if post_type == "/upload":
+            print('upload')
+            handle_upload(client_socket, headers, query_params, request_data)
+        elif post_type == "/delete":
+            print('delete')
+            handle_delete(client_socket, headers, path, request_data)
+        else:
+            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+            print('bad 401')
+            client_socket.send(response.encode('utf-8'))
+            return
+
+    except Exception as e:
+        # 处理任何异常并发送适当的响应
+        response = 'HTTP/1.1 500 Internal Server Error\r\n\r\n'
+        client_socket.send(response.encode('utf-8'))
+        print(f"处理POST请求时出错: {e}")
 
 
 def handle_client(client_socket):
@@ -181,6 +298,7 @@ def handle_client(client_socket):
 
         while True:
             request_data = client_socket.recv(1024).decode('utf-8')
+            #print(request_data)
             if not request_data:
                 break
 
@@ -218,12 +336,14 @@ def handle_client(client_socket):
             # Call the appropriate function based on the request
                 if method == 'GET':
                     print('get get')
-                  # print(request_data)
+                    print(request_data)
                     handle_get(request_data,client_socket)
                 elif method == 'POST':
                     print('get post')
                     print(request_data)
-                    pass
+                    handle_post(client_socket, headers, path, request_data)
+                else:
+                    print('bad request')
                 # Add more methods as needed (e.g., DELETE, PUT)
 
                 # If Connection: Close, close the connection
